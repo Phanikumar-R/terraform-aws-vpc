59a942da183e3c95257267ec2665420371d16eae
phani <vsphanikumar777@gmail.com>
2026-03-12 23:23:00 +0530

    vpc actions

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ resource "aws_internet_gateway" "main" {
 
   tags = local.igw_final_tags
 }
+
+# Create a public subnet
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
