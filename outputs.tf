0e7ef46007d33f511dd529cf9e1606ab9d53f76d
phani <vsphanikumar777@gmail.com>
2026-03-15 10:54:58 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,26 @@ output "azs_info" {
     value = data.aws_availability_zones.available
   
 }
+
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+
+output "public_subnet_ids" {
+
+    value = aws_subnet.public[*].id     # Step-5: Output the public subnet IDs
+  
+}
+
+output "private_subnet_ids" {
+
+    value = aws_subnet.private[*].id     # Step-5: Output the private subnet IDs
+  
+}
+
+output "database_subnet_ids" {
+
+    value = aws_subnet.database[*].id     # Step-5: Output the database subnet IDs
+  
+}
